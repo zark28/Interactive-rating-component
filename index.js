afeae7b5850e794fbc4c9e49edae72cc0969b6a5
zark28 <abdulrazakadams28@gmail.com>
2023-09-14 12:04:58 +0000

    update comments

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ let selectedRating=0
 
 // set active rating
 function setActiveRating(){
+  // remove all active classes
 ratingNumbers.forEach(rating=> rating.classList.remove('active'))
+// add active class on selected rating
 event.target.classList.add('active')
+// set rating value
 selectedRating = event.target.value
-// console.log(selectedRating );
 }
 
 // handle submit action
 function handleSubmit(){
+  // check is a rating is selected
     if(selectedRating <= 0){
         alert('Please Select a rating')
         return;
     }
+    // toggle active card component 
     container.firstElementChild.classList.toggle('active')
     container.lastElementChild.classList.toggle('active')
+    // add seleted rating value to html text
     displayRating.innerHTML = selectedRating
     // setTimeout( handleReset,6000)
 }
@@ -37,6 +42,9 @@ function handleReset(){
 
 
 // Add event listiners
+
+// listen to click on rating numbers
 ratingContainer.addEventListener('click',setActiveRating)
+// listen to click on submit button
 button.addEventListener('click',handleSubmit)
 
